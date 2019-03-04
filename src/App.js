73303774da59c985812e0f586f04f27337e365bf
studickie <dickie.stuart@gmail.com>
2019-03-04 17:01:47 -0500

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+// import componenets
 import React, { Component } from 'react';
+// import dependencies
+import axios from 'axios';
+import firebase from './firebase.js';
+// import styles
 import './App.css';
 
 class App extends Component {
 
   render() {
-    
+
     return (
       <div className="gifty-app">
       </div>
