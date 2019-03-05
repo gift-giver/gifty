ffdf533856a805d007617f1cbbb3fd270428b8fc
Dustin <dustinbradley45@live.com>
2019-03-05 11:40:44 -0500

    added proxy

--- src/App.js
+++ src/App.js
@@ -15,30 +15,32 @@ class App extends Component {
       itemDescription: ''
     }
   }
-  
-   
-  render() {
-    
-    axios.get('https://openapi.etsy.com/v2/listings/active', {
-      params: {
-        api_key: '4jabbvn0odt4iogwe763zl4m',
-        method: 'GET',
-        offset: 20,
-        limit: 20,
-        state: 'active',
-        keywords: 'pants'
-      }
-    })
-      .then((result) => {
-      
-        const itemDescription = result.data.results[1].title
-        this.setState({
-          itemDescription: itemDescription
-        })
 
-        const results = result.data.results[1].listing_id
+  componentDidMount() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const listingUrl = 'https://openapi.etsy.com/v2/listings/active'
 
-         axios.get('https://openapi.etsy.com/v2/listings/:listing_id/images/', {
+     axios.get(proxyUrl + listingUrl, {
+         params: {
+           api_key: '4jabbvn0odt4iogwe763zl4m',
+           method: 'GET',
+           offset: 20,
+           limit: 20,
+           state: 'active',
+           keywords: 'pants'
+         }
+       })
+       .then((result) => {
+
+         const itemDescription = result.data.results[1].title
+         this.setState({
+           itemDescription: itemDescription
+         })
+
+         const results = result.data.results[1].listing_id
+         const apiImgUrl = 'https://openapi.etsy.com/v2/listings/:listing_id/images/'
+
+         axios.get(proxyUrl + apiImgUrl, {
            params: {
              api_key: '4jabbvn0odt4iogwe763zl4m',
              method: 'GET',
@@ -50,8 +52,15 @@ class App extends Component {
              imageUrl: itemImage
            })
          })
-      })
+       })
+  }
   
+   
+  render() {
+
+
+
+   
   
 
     return (
