c13a420e79e6361327eb98a2a81a75a53fc2d9bb
Dustin <dustinbradley45@live.com>
2019-03-04 21:47:58 -0500

    API call

--- src/App.js
+++ src/App.js
@@ -6,12 +6,58 @@ import firebase from './firebase.js';
 // import styles
 import './App.css';
 
-class App extends Component {
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      imageUrl: '',
+      itemDescription: ''
+    }
+  }
+  
+   
   render() {
+    
+    axios.get('https://openapi.etsy.com/v2/listings/active', {
+      params: {
+        api_key: '4jabbvn0odt4iogwe763zl4m',
+        method: 'GET',
+        offset: 20,
+        limit: 20,
+        state: 'active',
+        keywords: 'pants'
+      }
+    })
+      .then((result) => {
+      
+        const itemDescription = result.data.results[1].title
+        this.setState({
+          itemDescription: itemDescription
+        })
+
+        const results = result.data.results[1].listing_id
+
+         axios.get('https://openapi.etsy.com/v2/listings/:listing_id/images/', {
+           params: {
+             api_key: '4jabbvn0odt4iogwe763zl4m',
+             method: 'GET',
+             listing_id: results,
+           }
+         }).then((data) => {
+           const itemImage = data.data.results[0].url_fullxfull
+           this.setState({
+             imageUrl: itemImage
+           })
+         })
+      })
+  
+  
 
     return (
-      <div className="gifty-app">
+      <div className="gifty-app" >
+        <img src={this.state.imageUrl}></img>
+        <h2>{this.state.itemDescription}</h2>
       </div>
     );
   }
