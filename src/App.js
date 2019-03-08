58948afadba81f82fa7f97baafc50a49b4d51652
studickie <dickie.stuart@gmail.com>
2019-03-07 19:48:40 -0500

    testing things

--- src/App.js
+++ src/App.js
@@ -3,23 +3,55 @@ import React, { Component } from 'react';
 import MainApp from './Components/MainApp.js';
 // import LoginPage from './Components/LoginPage.js';
 // import router
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom';
 import firebase from './firebase.js';
 // import styles
 import './App.css'; 
-
+// firebase auth stuff
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
 
 class App extends Component {
   constructor() {
     super();
     
     this.state = {
-      user: "WeAreTheUser.. NotThem..Us"
+      user: null,
+      loggedIn: false
     }
   }
 
   componentDidMount(){
 
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ 
+          user: user
+        });
+      }
+    });
+  }
+
+  login = () => {
+
+    auth.signInWithPopup(provider)
+      .then((result) => {
+        const user = result.user;
+        this.setState({
+          user: user,
+          loggedIn: true
+        });
+      });
+  }
+
+  logout = () => {
+
+    auth.signOut()
+      .then(() => {
+        this.setState({
+          user: null
+        });
+      });
   }
   
   
@@ -29,12 +61,13 @@ class App extends Component {
         <div>
           <header>
 
-            <Link to="MainApp">Login</Link>
-          <Link to="MainApp">Guest</Link>
+            <Link to="MainApp">Guest</Link>
 
+            {this.state.user ? <button onClick={this.logout}>Log Out</button> : <button onClick={this.login}>Login</button>}
           </header>
-
-          <Route path="/MainApp" exact render = { () => {return (<MainApp name={this.state.user}/>)} } />
+          <Route path="/MainApp" exact render={() => { return (<MainApp name={this.state.user} />) }} />
+          <Route exact path="/MainApp" render={() => (this.state.loggedIn ? (<Redirect to="/MainApp" />) : (<App />))} />
+          
         </div>
       </Router>
     )
