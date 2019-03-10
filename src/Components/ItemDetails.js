814e26d2ce57ee032dc9d80dd5188067e23f3a36
Alanna <oconnor.alannae@gmail.com>
2019-03-10 15:28:36 -0400

    revised router functions

--- src/Components/ItemDetails.js
+++ src/Components/ItemDetails.js
@@ -1,51 +1,27 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import MyList from './MyList.js';
-import firebase from './../firebase.js'
+import React from 'react';
 
-class ItemDetails extends Component {
-    constructor(){
-        super();
-
-        this.state = {
-
-        }
-    }
-
-   
-
-
-    render(){
-        return (
+const ItemDetails = (props) => {
+ return (
             <React.Fragment>
                 <div className="backgroundBlur"></div>
                 <div className="itemModal">
                     <button className="closeModal"
-                        onClick={this.props.onModalClose}>
+                        onClick={props.onModalClose}>
                         X</button> 
-                    <img src={this.props.itemInfo.image_url} alt={this.props.itemInfo.name}></img>
+                    <img src={props.itemInfo.image_url} alt={props.itemInfo.name}/>
+
                     <div className='modalInfoContainer'>
-                        <h2>{this.props.itemInfo.name}</h2>
-                        <p>{this.props.itemInfo.rating}</p>
+                        <h2>{props.itemInfo.name}</h2>
+                        <p>{props.itemInfo.rating}</p>
                       
-                        <button onClick={() => this.props.pushToFirebase(this.props.itemInfo)} key={this.props.itemInfo.id}>Add To List</button>
+                        <button onClick={() => props.pushToFirebase(props.itemInfo)} key={props.itemInfo.id}>Add To List</button>
                    
                     </div>
-              
                     
                 </div>
             </React.Fragment>
         )
-    }
 }
 
-//   price: place.price,
-//       image: place.image_url,
-//       category: place.category,
-//       url: place.url,
-//       rating: place.rating,
-//       name: place.name,
-//       id: place.id,
-//       address: place.location.displayAddress
 
 export default ItemDetails;
